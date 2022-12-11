f79be56ce48beb0784ab730213e977a157e0cdd0
Bharat Rav <bharatrav0101@gmail.com>
2022-12-11 19:00:01 +0530

    POTD added

---
+++ src/POTD/Dec_11_22/ArrayPairSumDivisibilityProblem.java
@@ -0,0 +1,41 @@
+package POTD.Dec_11_22;
+
+public class ArrayPairSumDivisibilityProblem {
+    public boolean canPair(int[] nums, int k) {
+
+        // Code here
+
+        if(nums.length%2==1) {
+
+            return false;
+
+        }
+
+        int mem[] = new int[k];
+
+        for(int x : nums)
+
+        {
+
+            mem[x%k]++;
+
+        }
+
+        if(mem[0]%2==1){
+
+            return false;
+
+        }
+
+        for(int i=1; i<=k/2; i++)
+
+        {
+
+            if(mem[i]!=mem[k-i]) return false;
+
+        }
+
+        return true;
+
+    }
+}
